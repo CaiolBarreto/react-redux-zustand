4cd580fc26263b2ab84d358b7c661c946466bc21
Caio Barreto <caiobarreto_28@hotmail.com>
2023-11-07 18:20:06 -0300

    creating global hook selector

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,20 +1,11 @@
 import ReactPlayer from 'react-player'
-import { useAppSelector } from '../store'
 import { useDispatch } from 'react-redux'
-import { next } from '../store/slices/player'
+import { next, useCurrentLesson } from '../store/slices/player'
 
 export function Video() {
   const dispatch = useDispatch()
 
-  const lesson = useAppSelector(state => {
-
-    const { currentModuleIndex, currentLessonIndex } = state.player
-
-    const currentLesson =
-      state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex]
-
-    return currentLesson
-  })
+  const { currentLesson } = useCurrentLesson()
 
   function handleNextVideo() {
     dispatch(next())
@@ -23,12 +14,11 @@ export function Video() {
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <ReactPlayer
-        url={`https://www.youtube.com/watch?v=${lesson.id}`}
+        url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
         onEnded={handleNextVideo}
         width="100%"
         height="100%"
         controls
-        playing
       />
     </div>
   );
